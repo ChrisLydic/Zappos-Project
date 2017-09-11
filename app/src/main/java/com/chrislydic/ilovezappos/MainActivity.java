c51712ecd8026692838ec9076b508c66198b1369
Chris Lydic <chris@chrislydic.com>
2017-09-11 12:15:12 -0400

    Started work on price alert feature

--- app/src/main/java/com/chrislydic/ilovezappos/MainActivity.java
+++ app/src/main/java/com/chrislydic/ilovezappos/MainActivity.java
@@ -11,6 +11,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
@@ -62,7 +70,50 @@ public class MainActivity extends AppCompatActivity {
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if ( id == R.id.action_settings ) {
+		if ( id == R.id.action_set_alert ) {
+			//https://github.com/firebase/firebase-jobdispatcher-android TODO remove this line when done
+			// Create a new dispatcher using the Google Play driver.
+			FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+
+			Bundle myExtrasBundle = new Bundle();
+			myExtrasBundle.putString("some_key", "some_value");
+
+			Job myJob = dispatcher.newJobBuilder()
+					// the JobService that will be called
+					.setService(PriceAlertService.class)
+					// uniquely identifies the job
+					.setTag("my-unique-tag")
+					// one-off job
+					.setRecurring(false)
+					// don't persist past a device reboot
+					.setLifetime( Lifetime.UNTIL_NEXT_BOOT)
+					// start between 0 and 60 seconds from now
+					.setTrigger( Trigger.executionWindow(0, 60))
+					// don't overwrite an existing job with the same tag
+					.setReplaceCurrent(false)
+					// retry with exponential backoff
+					.setRetryStrategy( RetryStrategy.DEFAULT_EXPONENTIAL)
+					// constraints that need to be satisfied for the job to run
+					.setConstraints(
+							// only run on an unmetered network
+							Constraint.ON_UNMETERED_NETWORK,
+							// only run when the device is charging
+							Constraint.DEVICE_CHARGING
+					)
+					.setExtras(myExtrasBundle)
+					.build();
+
+			dispatcher.mustSchedule(myJob);
+
+			//dispatcher.cancel("my-unique-tag");
+
+			invalidateOptionsMenu();
+			return true;
+		} else if ( id == R.id.action_update_alert ) {
+
+			return true;
+		} else if ( id == R.id.action_remove_alert ) {
+			invalidateOptionsMenu();
 			return true;
 		}
 
