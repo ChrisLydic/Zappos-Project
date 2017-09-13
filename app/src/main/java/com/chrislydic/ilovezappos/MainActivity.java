9848913d588ab3e1d7d543b614d986ba807b4526
Chris Lydic <chris@chrislydic.com>
2017-09-12 23:33:07 -0400

    more work on price alert

--- app/src/main/java/com/chrislydic/ilovezappos/MainActivity.java
+++ app/src/main/java/com/chrislydic/ilovezappos/MainActivity.java
@@ -1,15 +1,19 @@
 package com.chrislydic.ilovezappos;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.firebase.jobdispatcher.Constraint;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
@@ -71,46 +75,41 @@ public class MainActivity extends AppCompatActivity {
 
 		//noinspection SimplifiableIfStatement
 		if ( id == R.id.action_set_alert ) {
-			//https://github.com/firebase/firebase-jobdispatcher-android TODO remove this line when done
-			// Create a new dispatcher using the Google Play driver.
-			FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
 
-			Bundle myExtrasBundle = new Bundle();
-			myExtrasBundle.putString("some_key", "some_value");
-
-			Job myJob = dispatcher.newJobBuilder()
-					// the JobService that will be called
-					.setService(PriceAlertService.class)
-					// uniquely identifies the job
-					.setTag("my-unique-tag")
-					// one-off job
-					.setRecurring(false)
-					// don't persist past a device reboot
-					.setLifetime( Lifetime.UNTIL_NEXT_BOOT)
-					// start between 0 and 60 seconds from now
-					.setTrigger( Trigger.executionWindow(0, 60))
-					// don't overwrite an existing job with the same tag
-					.setReplaceCurrent(false)
-					// retry with exponential backoff
-					.setRetryStrategy( RetryStrategy.DEFAULT_EXPONENTIAL)
-					// constraints that need to be satisfied for the job to run
-					.setConstraints(
-							// only run on an unmetered network
-							Constraint.ON_UNMETERED_NETWORK,
-							// only run when the device is charging
-							Constraint.DEVICE_CHARGING
-					)
-					.setExtras(myExtrasBundle)
-					.build();
-
-			dispatcher.mustSchedule(myJob);
-
-			//dispatcher.cancel("my-unique-tag");
 
 			invalidateOptionsMenu();
 			return true;
 		} else if ( id == R.id.action_update_alert ) {
+			LayoutInflater li = LayoutInflater.from(this);
+			View promptsView = li.inflate(R.layout.price_prompt, null);
 
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			alertDialogBuilder.setView( promptsView );
+			alertDialogBuilder.setCancelable(false);
+
+			final EditText priceAlertInput = (EditText) promptsView
+					.findViewById(R.id.price_input);
+
+			alertDialogBuilder.setPositiveButton(
+					"OK",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							//dispatcher.cancel("my-unique-tag");
+							createPriceAlert( Integer.parseInt(priceAlertInput.getText().toString()) );
+							dialog.cancel();
+						}
+					});
+
+			alertDialogBuilder.setNegativeButton(
+					"Cancel",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							dialog.cancel();
+						}
+					});
+
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			alertDialog.show();
 			return true;
 		} else if ( id == R.id.action_remove_alert ) {
 			invalidateOptionsMenu();
@@ -118,5 +117,37 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		return super.onOptionsItemSelected( item );
+	}
+
+	private void createPriceAlert(int priceFloor) {
+		// Create a new dispatcher using the Google Play driver.
+		FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+
+		Job myJob = dispatcher.newJobBuilder()
+				// the JobService that will be called
+				.setService(PriceAlertService.class)
+				// uniquely identifies the job
+				.setTag("my-unique-tag")
+				// one-off job
+				.setRecurring(false)
+				// don't persist past a device reboot
+				.setLifetime( Lifetime.UNTIL_NEXT_BOOT)
+				// start between 0 and 60 seconds from now
+				.setTrigger( Trigger.executionWindow(0, 60))
+				// don't overwrite an existing job with the same tag
+				.setReplaceCurrent(false)
+				// retry with exponential backoff
+				.setRetryStrategy( RetryStrategy.DEFAULT_EXPONENTIAL)
+				// constraints that need to be satisfied for the job to run
+				.setConstraints(
+						// only run on an unmetered network
+						Constraint.ON_UNMETERED_NETWORK,
+						// only run when the device is charging
+						Constraint.DEVICE_CHARGING
+				)
+				.setExtras(PriceAlertService.getBundle( priceFloor ))
+				.build();
+
+		dispatcher.mustSchedule(myJob);
 	}
 }
--- app/src/main/java/com/chrislydic/ilovezappos/PriceAlertService.java
+++ app/src/main/java/com/chrislydic/ilovezappos/PriceAlertService.java
@@ -1,18 +1,96 @@
 package com.chrislydic.ilovezappos;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class PriceAlertService extends JobService {
+	private static final String TAG = "PriceAlertService";
+	private static final String ARG_PRICE = "price";
+
+	public static Bundle getBundle( int price ) {
+		Bundle args = new Bundle();
+		args.putSerializable( ARG_PRICE, price );
+		return args;
+	}
+
 	@Override
 	public boolean onStartJob(JobParameters job) {
-		// Do some work here
+		int price = job.getExtras().getInt( ARG_PRICE );
 
-		return false; // Answers the question: "Is there still work going on?"
+		new FetchPriceTask().execute( price );
+
+		return true;
 	}
 
 	@Override
 	public boolean onStopJob(JobParameters job) {
-		return false; // Answers the question: "Should this job be retried?"
+		return true;
+	}
+
+	private class FetchPriceTask extends AsyncTask<Integer, Void, Boolean> {
+		@Override
+		protected Boolean doInBackground( Integer... terms ) {
+			JSONObject result = null;
+			try {
+				result = new BitstampAPI().getPrice();
+			} catch ( IOException exc ) {
+				Log.e( TAG, "Failed to fetch URL: ", exc );
+			}
+
+			if ( result != null ) {
+				try {
+					if (terms[0] < result.getDouble( "last" )) {
+						return true;
+					}
+				} catch ( JSONException exc ) {
+					Log.e( TAG, "Failed to parse json" );
+				}
+			}
+
+			return false;
+		}
+
+		@Override
+		protected void onPostExecute( Boolean price ) {
+			NotificationCompat.Builder mBuilder =
+					new NotificationCompat.Builder(getApplicationContext())
+							.setSmallIcon(R.drawable.border_bottom_row)
+							.setContentTitle("My notification")
+							.setContentText("Hello World!");
+
+			Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+			notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+					| Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+			PendingIntent intent = PendingIntent.getActivity(getApplicationContext(), 0,
+					notificationIntent, 0);
+
+			mBuilder.setContentIntent(intent);
+
+
+			// Sets an ID for the notification
+			int mNotificationId = 001;
+			// Gets an instance of the NotificationManager service
+			NotificationManager mNotifyMgr =
+					(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+			// Builds the notification and issues it.
+			mNotifyMgr.notify(mNotificationId, mBuilder.build());
+
+
+		}
 	}
 }
